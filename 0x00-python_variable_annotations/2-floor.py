67b90e6f07b66c9fb6aa8d627e058ec5c43d9a12
Fati-Zid <ezzaidanif@gmail.com>
2024-06-07 01:05:11 +0000

    type annotation: return int from a float

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,1 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+	function that return floor of a floot
+	exemple: 3.14 -> 3
+"""
+def floor(a: float)-> int:
+	"""return the number on an int"""
+	return int(a)
+
