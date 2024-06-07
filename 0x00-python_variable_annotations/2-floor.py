ac21f946b24c7132fff915de4271f240a852c1e4
Fati-Zid <ezzaidanif@gmail.com>
2024-06-07 01:06:56 +0000

    update var

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -3,7 +3,7 @@
 	function that return floor of a floot
 	exemple: 3.14 -> 3
 """
-def floor(a: float)-> int:
+def floor(n: float)-> int:
 	"""return the number on an int"""
-	return int(a)
+	return int(n)
 
