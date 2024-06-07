453945095748a51356a05ee167b7009cbbab700e
Fati-Zid <ezzaidanif@gmail.com>
2024-06-07 02:16:48 +0000

    type anotation complete mondatory tasl

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,1 +1,12 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+Basic annotations - to string
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    cast float to string
+    """
+    return str(n)
+
--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,1 +1,13 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+Complex types - list of floats
+"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    return a sum of all nums inside a list
+    """
+    return sum(input_list)
+
